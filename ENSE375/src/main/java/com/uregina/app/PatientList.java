f485c1c1974d314d0f88233357c7680dd7dd12c3
KadenGoski <55845863+KadenGoski@users.noreply.github.com>
2021-03-08 15:04:48 -0600

    PatientList Updated
    Need to test out.

--- ENSE375/src/main/java/com/uregina/app/PatientList.java
+++ ENSE375/src/main/java/com/uregina/app/PatientList.java
@@ -2,39 +2,64 @@ package com.uregina.app;
 import com.uregina.exception.*;
 import java.util.ArrayList;
 
-public class PatientList 
+//ENSE 375 - March 12th, 2021
+//Project Step 3 CI - Part 2
+//GROUP A
+//Kaden Goski - 200392031
+//
+public class PatientList
 {
-	private ArrayList<Patient> patientList;
-    	public PatientList() 
-    	{
-    		//ToDo: add you code here
-    	}
-    	/**
-	*
-	* @return boolean: true if the patient can be added to the list 
-	*/
-    	public boolean addPatient(Patient patient)
-    	{
-    		//ToDo: add you code here
-    		return true;
-    	}
-    	/**
-	*
-	* @return boolean: true if the patient is removed from the list 
-	*/
-    	public boolean deletePatient(int index)
-    	{
-    		//ToDo: add you code here
-    		return true;
-    	}
-    	public Patient getPatient(String ID)
-    	{
-    		//ToDo: add you code here
-    		return null;
-    	}
-    	public int getNumberofPatients()
-    	{
-    		//ToDo: add you code here (you can update the return statement)
-    		return 0;
-    	}
+    private ArrayList<Patient> patientList;
+    public PatientList()
+    {
+        //ToDo: add you code here
+        this.patientList = new ArrayList<>();
+    }
+    /**
+     *
+     * @return boolean: true if the patient can be added to the list
+     */
+    public boolean addPatient(Patient patient)
+    {
+        //ToDo: add you code here
+        patientList.add(patient);
+        return true;
+    }
+    /**
+     *
+     * @return boolean: true if the patient is removed from the list
+     */
+    public boolean deletePatient(int index)
+    {
+        //ToDo: add you code here
+        //index starts at 0, size starts at 1. minus 1 from size.
+        if (patientList.size()-1 <= index)
+        {
+            patientList.remove(index);
+            return true;
+        }
+        return false;
+    }
+    public Patient getPatient(String ID)
+    {
+        //ToDo: add you code here
+        //ID must be 9 long and have atleast 1 non zero digit, cant be all zeroes.
+        //could use throw exception?
+        if ((ID.length() == 9) && (!(ID.equals("000000000")))) {
+            //Go through the list
+            for (int i = 0; i < patientList.size(); i++) {
+                //If ID equals the patient id from the patient at the index position in the list
+                if (ID.equals(patientList.get(i).getID())) {
+                    //return the patient that matches.
+                    return (patientList.get(i));
+                }
+            }
+        }
+        return null;
+    }
+    public int getNumberofPatients()
+    {
+        //ToDo: add you code here (you can update the return statement)
+        return patientList.size();
+    }
 }
