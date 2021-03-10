844c9517b2e0d823db13a3386cafb556c924908b
Roxanne Harrison <harrirox@uregina.ca>
2021-03-09 19:50:23 -0600

    completed PatientHistogram.java

--- ENSE375/src/main/java/com/uregina/app/PatientHistogram.java
+++ ENSE375/src/main/java/com/uregina/app/PatientHistogram.java
@@ -2,21 +2,30 @@ package com.uregina.app;
 import com.uregina.exception.*;
 import java.util.ArrayList;
 
+//ENSE 375 - March 12th, 2021
+//Project Step 3 CI - Part 2
+//GROUP A
+//Roxanne Harrison - 200333074
+//
 public class PatientHistogram 
 {
 	private int[][] patientCount;
     	public PatientHistogram() 
     	{
-    		//ToDo: add you code here
+			// THE CITY SHAPE IS ASSUMED TO BE A RECTANGLE WHICH IS DIVIDED INTO
+			// 		20x10 REGIONS
+			this.patientCount = new int[20][10];
     	}
-    	/**
+    /**
 	*
 	* @return boolean: true if the a patient can be added from that region 
 	*/
     	public boolean addAPatientToRegion(int VIndex,int HIndex)
     	{
-    		//ToDo: add you code here
-    		
+			// INCREMENT THE COUNT IN THAT REGION
+			int count = patientCount[VIndex][HIndex];
+			count++;
+			patientCount[VIndex][HIndex] = count;
     		return true;
     	}
 	/**
@@ -25,13 +34,15 @@ public class PatientHistogram
 	*/
     	public boolean deleteAPatientFromRegion(int VIndex,int HIndex)
     	{
-    		//ToDo: add you code here 
+    		// DECREMENT THE COUNT IN THAT REGION
+			int count = patientCount[VIndex][HIndex];
+			count--;
+			patientCount[VIndex][HIndex] = count;
     		
     		return true;
     	}
     	public int getPatientsCountInRegion(int VIndex,int HIndex) throws IndexOutOfBoundsException
     	{
-    		//ToDo: add you code here (you can update the return statement)
-    		return 0;
+    		return patientCount[VIndex][HIndex];
     	}
 }
