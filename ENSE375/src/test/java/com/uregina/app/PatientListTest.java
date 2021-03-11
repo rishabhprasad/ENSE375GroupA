97c595f69af32a2b33d547e3c1981e8e8c468790
KadenGoski <55845863+KadenGoski@users.noreply.github.com>
2021-03-11 17:25:57 -0600

    PatientListTest

--- ENSE375/src/test/java/com/uregina/app/PatientListTest.java
+++ ENSE375/src/test/java/com/uregina/app/PatientListTest.java
@@ -8,6 +8,8 @@ import com.uregina.exception.*;
 
 import org.junit.Test;
 
+import jdk.jfr.Timestamp;
+
 /**
  * Unit test for PatientList.
  */
@@ -17,17 +19,16 @@ public class PatientListTest
      * Rigorous Test :-)
      */
     @Test
-    public void patientList_FailToAdd()
+    public void patientList_Fail_To_Add()
     {
         PatientList patientList = new PatientList();
         //error in name.     
         assertFalse(patientList.addPatient(null));          
     }
     @Test
-    public void patientList_SuccesfulAdd() 
+    public void patientList_Succesful_Add() 
     {
         PatientList patientList = new PatientList();
-        boolean error = false;
 
         try{
         PostalCode postal = new PostalCode("K1A-0B9");
@@ -36,24 +37,159 @@ public class PatientListTest
         }
         catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
         {
-            error = true;
             assertTrue(false);
         }
      
     }
+    
     @Test
-    public void patientList_FailToAdd_InvalidPostal()
+    public void deletePatient_True()
     {
-        assertTrue( true );
+        PatientList patientList = new PatientList();
+
+        try{
+        PostalCode postal = new PostalCode("K1A-0B9");
+        Patient object = new Patient("Kaden","12345678",12,postal);
+        patientList.addPatient(object);
+        assertTrue(patientList.deletePatient(0));
+        }
+        catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
+        {
+            assertTrue(false);
+        }
+      
     }
     @Test
-    public void ithTrue()
+    public void deletePatient_Fail()
     {
-        assertTrue( true );
+        PatientList patientList = new PatientList();
+
+        try{
+        PostalCode postal = new PostalCode("K1A-0B9");
+        Patient object = new Patient("Kaden","12345678",12,postal);
+        patientList.addPatient(object);
+        assertFalse(patientList.deletePatient(2));
+        }
+        catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
+        {
+            assertFalse(true);
+        }
     }
+
     @Test
-    public void shouldAnswWithTrue()
+    public void getPatient_InvalidID_Fail()
     {
-        assertTrue( true );
+    PatientList patientList = new PatientList(); 
+    boolean result = false;
+
+    try{
+    PostalCode postal = new PostalCode("K1A-0B9");
+    Patient object = new Patient("Kaden","12345678",12,postal);
+    patientList.addPatient(object);
+
+    if (patientList.getPatient("000000000") == null)
+    {
+    result = true;
     }
+
+    assertTrue(result);
+    }
+
+    catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
+    {
+        error = true;
+        assertTrue(false);
+    }
+    }
+
+    @Test
+    //No patient with ID fail..
+    public void getPatient_ID_Fail()
+    {
+    PatientList patientList = new PatientList();
+    boolean result = false;
+
+    try{
+    PostalCode postal = new PostalCode("K1A-0B9");
+    Patient object = new Patient("Kaden","12345678",12,postal);
+    patientList.addPatient(object);
+
+    if (patientList.getPatient("000000022") == null)
+    {
+        result = true;
+    }
+    
+    assertTrue(result);
+    }
+    catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
+    {
+        assertTrue(false);
+    }
+    }
+
+
+    @Test
+    //No patient with ID fail..
+    public void getPatient_ID_Success()
+    {
+    PatientList patientList = new PatientList();
+    boolean result = true;
+
+    try{
+    PostalCode postal = new PostalCode("K1A-0B9");
+    Patient object = new Patient("Kaden","12345678",12,postal);
+    patientList.addPatient(object);
+    
+    Patient patientX = patientList.getPatient("12345678");
+    if (patientX == null)
+    {
+        result = false;
+    }
+    
+    assertTrue(result);
+    }
+    catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
+    {
+        assertTrue(false);
+    }
+    }
+
+    //2
+    @Test
+    public void getNumOfPatients_Success()
+    {
+        PatientList patientList = new PatientList();
+        
+        PostalCode postal = new PostalCode("K1A-0B9");
+        Patient object = new Patient("Kaden","12345678",12,postal);
+        patientList.addPatient(object);
+        PostalCode postal2 = new PostalCode("K1A-0C9");
+        Patient object2 = new Patient("KadenG","12345679",14,postal2);
+        patientList.addPatient(object2);
+
+
+        int number = patientList.getNumberofPatients();
+
+        if(number == 2)
+        {
+            assertTrue(true);
+        }
+        assertTrue(false);
+    }
+
+    @Test
+    public void getNumOfPatients_Empty()
+    {
+        PatientList patientList = new PatientList();
+
+        int number = patientList.getNumberofPatients();
+
+        if(number == 0)
+        {
+            assertTrue(true);
+        }
+        assertTrue(false);
+    }
+
+
 }
