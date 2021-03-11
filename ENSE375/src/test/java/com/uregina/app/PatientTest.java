b79b949acd1ad428ee5b4b7211d97ba550078d8d
kylechen1994@hotmail.com <kylechen1994@hotmail.com>
2021-03-11 14:43:55 -0600

    update Patient

--- ENSE375/src/test/java/com/uregina/app/PatientTest.java
+++ ENSE375/src/test/java/com/uregina/app/PatientTest.java
@@ -29,39 +29,64 @@ public class PatientTest
     @Test
     public void Correct_age_False_unequal()
     {
+                try{
             PostalCode postalCode = new PostalCode("K1A-0B9");
             Patient patient = new Patient("Landry", "000000000",12,postalCode);
             assertFalse(patient.setAge(13));
+        }
+        catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
+        {
+            assertTrue(false);
+        }
     }
     @Test
     public void Correct_age_False_outOf_Range()
-    {
+    {        try{
             PostalCode postalCode = new PostalCode("K1A-0B9");
             Patient patient = new Patient("Landry", "000000000",120,postalCode);
             assertFalse(patient.setAge(120));
+                }
+            catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
+        {
+            assertTrue(false);
+        }
     }
     @Test
     public void Correct_postalCode_True()
     {
+        try{
             PostalCode postalCode = new PostalCode("K1A-0B9");
             PostalCode postalc = new PostalCode("K1A-0B9");
             Patient patient = new Patient("Landry", "000000000",12,postalCode);
             assertTrue(patient.setPostalCode(postalc));
+        }
+        catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
+        {
+            assertTrue(true);
+        }
     }
     @Test
     public void Correct_postalCode_False_incorecte_input()
-    {
+    {        try{
             PostalCode postalCode = new PostalCode("K1A_0B9");
             PostalCode postalc = new PostalCode("K1A_0B9");
             Patient patient = new Patient("Landry", "000000000",12,postalCode);
             assertFalse(patient.setPostalCode(postalc));
+        } catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
+        {
+            assertTrue(false);
+        }
     }
     @Test
     public void Correct_postalCode_False_unequal()
-    {
+    {        try{
             PostalCode postalCode = new PostalCode("K1A-0B9");
             PostalCode postalc=new PostalCode("K1A-0B6");
             Patient patient = new Patient("Landry", "000000000",12,postalCode);
             assertFalse(patient.setPostalCode(postalc));
+          }catch (InvalidPostalCodeException | InvalidNameException | InvalidAgeException | InvalidIDException e)
+        {
+            assertTrue(false);
+        }
     }
 }
