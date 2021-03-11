3550b15965a6ed0fce659082aab7ca4b7360ea08
kylechen1994@hotmail.com <kylechen1994@hotmail.com>
2021-03-11 10:50:29 -0600

    debugging

--- ENSE375/src/main/java/com/uregina/app/PostalCode.java
+++ ENSE375/src/main/java/com/uregina/app/PostalCode.java
@@ -6,53 +6,49 @@ public class PostalCode
 	private String postalCode;
     	public static boolean isValidPostalCode(String postalCode)
     	{
-		  int postalCodeLength = 7;
+			int postalCodeLength = 7;
 
 		
-		  if (postalCode.length() != postalCodeLength) {
-			  return false;
-		  }
-
-		  String fixedPrefix = postalCode.substring(0, 2);
-		  char verticalIndex = postalCode.charAt(2);
-		  char separator = postalCode.charAt(3);
-		  int horizontalIndex = postalCode.charAt(4);
-		  char upperCaseLetter = postalCode.charAt(5);
-		  int digit = postalCode.charAt(6);
-
-		  if (!fixedPrefix.contains("K1")) {
-			  return false;
-		  }
-
-		  if (verticalIndex < 'A' || verticalIndex > 'T') {
-		  	  return false;
-		  }
-
-		  if (separator != '-') {
-			  return false;
-		  }
-
-		  if (horizontalIndex < '0' || horizontalIndex > '9') {
-			  return false;
-		  }
-
-		  if (upperCaseLetter < 'A' || upperCaseLetter > 'Z') {
-			  return false;
-		  }
-
-		  if (digit < '0' || digit > '9') {
-			  return false;
-		  }
-
-       	return true;
+			if (postalCode.length() != postalCodeLength) {
+				return false;
+			}
+  
+			String fixedPrefix = postalCode.substring(0, 2);
+			char verticalIndex = postalCode.charAt(2);
+			char separator = postalCode.charAt(3);
+			int horizontalIndex = postalCode.charAt(4);
+			char upperCaseLetter = postalCode.charAt(5);
+			int digit = postalCode.charAt(6);
+  
+			if (!fixedPrefix.contains("K1")) {
+				return false;
+			}
+  
+			if (verticalIndex < 'A' || verticalIndex > 'T') {
+				  return false;
+			}
+  
+			if (separator != '-') {
+				return false;
+			}
+  
+			if (horizontalIndex < '0' || horizontalIndex > '9') {
+				return false;
+			}
+  
+			if (upperCaseLetter < 'A' || upperCaseLetter > 'Z') {
+				return false;
+			}
+  
+			if (digit < '0' || digit > '9') {
+				return false;
+			}
+  
+			 return true;
     	}
     	public PostalCode(String postalCode) throws InvalidPostalCodeException
     	{	
-			if(isValidPostalCode(postalCode))
-			{
-    		this.postalCode = postalCode;	
-			}
-					
+    		this.postalCode = postalCode;				
     	}
     	public int getRegionVerticalIndex()
     	{
