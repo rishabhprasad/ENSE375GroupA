ea1ecd74507d64754d15e1faf9d9c452133ca31d
Zhuo Chen <55067409+ZhuoChenP@users.noreply.github.com>
2021-03-11 18:26:06 -0600

    update

--- ENSE375/src/main/java/com/uregina/app/Patient.java
+++ ENSE375/src/main/java/com/uregina/app/Patient.java
@@ -1,6 +1,6 @@
 package com.uregina.app;
 import com.uregina.exception.*;
-import java.util.regex.*;
+
 
 public class Patient 
 {
@@ -10,42 +10,27 @@ public class Patient
 	private PostalCode postalCode;
 	
 	public Patient(String name, String ID, int age, PostalCode postalCode) throws InvalidNameException, InvalidAgeException ,InvalidIDException, InvalidPostalCodeException
-	{		
-		 //The patient ID is a 9-digit string with a non-zero digit. 
-		 String IDCheck = "/(^[0-9]{9}$)/";
-		 Boolean IDFormat = Pattern.matches(IDCheck,ID);
- 
-		 //Patient name can only contain alphabetic letters as well as spaces and dots. 
-		 //Check if name is correct in format
-		 String nameCheck = "/^[a-z\\d\\-_\\s]+$/i";
-		 boolean isCorrectName = Pattern.matches(nameCheck, name);
- 
-		 if((age > 0 && age < 110))
-		 {
-		 if(IDFormat && isCorrectName && (postalCode != null))
-		 {
+	{
 		this.name=name;
 		this.ID=ID;
 		this.age=age;
-		this.postalCode= postalCode;
-		 }
-		}
+		this.postalCode=postalCode;
 	}
 	public String getName()
 	{
-		return this.name;
+		return name;
 	}
 	public String getID()
 	{
-		return this.ID;
+		return ID;
 	}
 	public int getAge()
 	{
-		return this.age;
+		return age;
 	}
 	public PostalCode getPostalCode()
 	{	
-		return this.postalCode;
+		return postalCode;
 	}
 	/**
 	*
@@ -53,10 +38,9 @@ public class Patient
 	*/
 	public boolean setAge(int age)
 	{	
-		if((age > 0 && age < 110))//compare set age with input age
+		if(this.age==age)//compare set age with input age
 		{
-			this.age = age;
-			return true;
+		return true;
 		}
 		return false;
 	}
@@ -66,11 +50,10 @@ public class Patient
 	*/
 	public boolean setPostalCode(PostalCode postalCode)
 	{
-		if(postalCode != null)//compare parameter with set postCode
+		if(this.postalCode.getPostalCode().equals(postalCode.getPostalCode()))//compare parameter with set postCode
 		{
-			this.postalCode = postalCode;
-				return true;
+		return true;
 		}
-	return false;
+		return false;
 	}
 }
