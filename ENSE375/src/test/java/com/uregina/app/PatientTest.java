7f15688d742585313148054b00cdc819e13a8dc5
KadenGoski <55845863+KadenGoski@users.noreply.github.com>
2021-03-11 19:27:19 -0600

    test

--- ENSE375/src/test/java/com/uregina/app/PatientTest.java
+++ ENSE375/src/test/java/com/uregina/app/PatientTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import com.uregina.exception.*;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.Test;
 
 /**
