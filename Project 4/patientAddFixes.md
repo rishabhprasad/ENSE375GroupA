3f3cf6b9a94183f4aaf9788a4ecaa4ff38041882
Bryden Trakalo <71333735+BrydenTrakalo@users.noreply.github.com>
2021-03-27 11:01:51 -0600

    fix postalcode, add more tests

--- Project 4/patientAddFixes.md
+++ Project 4/patientAddFixes.md
@@ -1,20 +1,24 @@
-To postal code initalization invalid exception catch
-'return false;'
+To postal code initalization invalid catch
+`return false;`
 
-to patient ititalization invalid id exception catch
-'return false;'
+to patient ititalization invalid id exception
+`return false;`
 
 to patient intialization
-'catch(NullPointerException e){
+```java
+catch(NullPointerException e){
     		System.out.println( "\tNull Pointer Exception" );
     		return false;
-    	}'
+    	}
+```
 
 
 To RiskCodeMAp.java
 Changed the functionaly of updateRiskInARegion to return true in all cases expect invalid inputs. 
 (used to return false if the risk code map was not updated in a call)
 
+To PostalCode.java
+`if (postalCode == null) throw new InvalidPostalCodeException();`
 
 TODO:
 should adding two patients with the same ID be illegal?
--- RiskMeter/src/test/java/com/uregina/app/AppTest.java
+++ RiskMeter/src/test/java/com/uregina/app/AppTest.java
@@ -42,6 +42,30 @@ public class AppTest
         assertFalse( valid );
     }
 
+    @Test
+    public void patientAdd_postalCode_invalidVerticalIndex()
+    {
+        App app = new App();
+        boolean valid = app.addPatient("Julie", "123456789", "K1Z-1C4", 31);
+        assertFalse( valid );
+    }
+
+    @Test
+    public void patientAdd_postalCode_invalidLowerCase()
+    {
+        App app = new App();
+        boolean valid = app.addPatient("Julie", "123456789", "K1T-3z4", 31);
+        assertFalse( valid );
+    }
+
+    @Test
+    public void patientAdd_postalCode_null()
+    {
+        App app = new App();
+        boolean valid = app.addPatient("Julie", "123456789", null, 31);
+        assertFalse( valid );
+    }
+
     //******************************PATIENT TESTS ******************************/
     @Test
     public void patientAdd_Invalid_Name()
@@ -60,6 +84,14 @@ public class AppTest
     }
 
     @Test
+    public void patientAdd_Invalid_ID_null()
+    {
+        App app = new App();
+        boolean valid = app.addPatient("Charlie", null, "K1A-0B9", 31);
+        assertFalse( valid );
+    }
+
+    @Test
     public void patientAdd_Invalid_ID()
     {
         App app = new App();
@@ -71,7 +103,7 @@ public class AppTest
     public void patientAdd_Invalid_ID_Character()
     {
         App app = new App();
-        boolean valid = app.addPatient("Julie", "000 00000", "K1A-0B9", 31);
+        boolean valid = app.addPatient("Julie", "780 00000", "K1A-0B9", 31);
         assertFalse( valid );
     }
 
@@ -83,13 +115,21 @@ public class AppTest
         assertFalse( valid );
     }
 
+    @Test
+    public void patientAdd_Invalid_Age2()
+    {
+        App app = new App();
+        boolean valid = app.addPatient("Julie", "123456789", "K1A-0B9", -1);
+        assertFalse( valid );
+    }
+
     //******************************PATIENT LIST TESTS ******************************/
     @Test
     public void patientAdd_2_patients()
     {
         App app = new App();
         app.addPatient("Julie", "123456789", "K1A-0B9", 31);
-        boolean valid = app.addPatient("George", "987654321", "K1A-0B9", 44);
+        boolean valid = app.addPatient("George", "987654321", "K1A-1B9", 44);
         assertTrue( valid );
     }
 
@@ -105,7 +145,13 @@ public class AppTest
 
     //******************************PatientHistogram TESTS ******************************/
 
-//******************************RiskCode map TESTS ******************************/
-
-
+    //******************************RiskCode map TESTS ******************************/
+    @Test
+    public void patientAdd_2_patients_same_region()
+    {
+        App app = new App();
+        app.addPatient("Julie", "123456789", "K1A-0B9", 31);
+        boolean valid = app.addPatient("George", "987654321", "K1A-0B9", 44);
+        assertTrue( valid );
+    }
 }
